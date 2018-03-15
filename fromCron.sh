ca614d2e21fc642c2de66434f535e833be47e30d
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-15 09:54:23 +0100

    initial commit tet

--- fromCron.sh
+++ fromCron.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-wget -O - https://raw.githubusercontent.com/ | sudo bash
+wget -O - https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh | sudo bash
 echo organisationName
 
 
