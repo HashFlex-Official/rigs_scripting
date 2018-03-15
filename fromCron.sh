c8acf9663e993129136029550c193e2d8076186b
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-15 09:59:50 +0100

    init

--- fromCron.sh
+++ fromCron.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 wget -O - https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh | bash
+organisationName2="test"
 echo $organisationName
+
+echo $organisationName2
 
 
