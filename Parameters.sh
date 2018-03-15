93dff7ef49182685ec729d4f4e214648e66ba474
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-15 10:53:37 +0100

    ## test

--- Parameters.sh
+++ Parameters.sh
@@ -1,3 +1,4 @@
-echo $organisationName
 export organisationName="HashFlex"
 export organisationName2="HashFlex"
+echo $organisationName
+echo $organisationName2
--- fromCron.sh
+++ fromCron.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
-export organisationName="Baaaaad"
 echo "#! /bin/bash" > ~/temp.sh
 curl https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh >> ~/temp.sh
-export organisationName2="test"
-sh ~/temp.sh
-echo $organisationName
-echo $organisationName2
-
 
 
 
