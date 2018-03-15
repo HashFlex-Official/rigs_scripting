72345746b3d96ff58d0dde863ad01acea53dc110
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-15 09:57:33 +0100

    initial commit again

--- fromCron.sh
+++ fromCron.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-wget -O - https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh | sudo bash
-echo organisationName
-
+wget -O - https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh | bash
+echo $organisationName
 
 
