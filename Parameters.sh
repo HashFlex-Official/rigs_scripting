7aa65a5a734456326d1a7d51b1bb0bcf159ee6e6
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-15 10:15:26 +0100

    test again and again

--- Parameters.sh
+++ Parameters.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
-
+echo $organisationName
 organisationName="HashFlex"
+organisationName2="HashFlex"
--- fromCron.sh
+++ fromCron.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
-wget -O - https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh | bash
+organisationName="Baaaaad"
+echo "#! /bin/bash" > ~/temp.sh
+curl https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh >> ~/temp.sh ; chmod +x ~/temp.sh
+sh ~/temp.sh
 
 organisationName2="test"
-
 echo $organisationName
-
 echo $organisationName2
+
+
+
 
 
