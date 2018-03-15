78c7abbfdacf13e2c4347e9b57745cc7a9960c28
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-15 10:51:09 +0100

    .... test

--- fromCron.sh
+++ fromCron.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 export organisationName="Baaaaad"
 echo "#! /bin/bash" > ~/temp.sh
-curl https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh | sh
-
+curl https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh >> ~/temp.sh
 export organisationName2="test"
+sh ~/temp.sh
 echo $organisationName
 echo $organisationName2
 
