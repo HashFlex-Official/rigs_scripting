cbd76ef8482f31cb58e5cbd019d0d86ef7636ef4
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-21 20:31:13 +0100

    test

--- fromCron.sh
+++ fromCron.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 echo "#! /bin/bash" > ~/temp.sh
 echo " "
-curl   -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh >> ~/temp.sh
+ wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh >> ~/temp.sh
 echo " "
-curl   -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/watchdog.sh >> ~/temp.sh
+ wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/watchdog.sh >> ~/temp.sh
 echo " "
-curl   -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Overclocking.sh >> ~/temp.sh
+ wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Overclocking.sh >> ~/temp.sh
 echo " "
-curl   -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/`hostname`_overcklocking.sh >> ~/temp.sh
+ wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/`hostname`_overcklocking.sh >> ~/temp.sh
 echo " "
 echo " "
 echo "overclocking_override" >> ~/temp.sh
