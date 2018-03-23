292c79770674d65b812e0a3249bce035ac2947ce
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-23 10:04:08 +0100

    working v1

--- fromCron.sh
+++ fromCron.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 ## wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/fromCron.sh | sh ; chmod +x ~/temp.sh ; sh ~/temp.sh  >> /tmp/watchdog.log 2>&1; rm ~/temp.sh
+
+export logPath="/tmp/watchdog.log"
+
 echo "#! /bin/sh" > ~/temp.sh
 echo " "
  wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Parameters.sh >> ~/temp.sh
  echo " "
  wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/`hostname`_parameters.sh >> ~/temp.sh
  echo " "
- sh ~/temp.sh
-echo " "
  wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/watchdog.sh >> ~/temp.sh
 echo " "
  wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/Overclocking.sh >> ~/temp.sh
