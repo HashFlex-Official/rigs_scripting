08264772a61af5e0a6ee3dfe139f7f713d20fe0f
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-03-23 07:52:55 +0100

    working v1 overclocking on hasflex2

--- hashflex-RIG02_parameters.sh
+++ hashflex-RIG02_parameters.sh
@@ -1,16 +1,16 @@
-export OverclockingOverrideEnabled=0
+export OverclockingOverrideEnabled=1
 # Check of overclocking is done on the voltageLimit
 # if we change memoryGPU1 but is we do not change the voltageLimit overclocking is not going to be applied
 export voltageLimitOverride="120"
 export memoryGPU1Override="1000"
-export clockGPU1Override="90"
+export clockGPU1Override="100"
 export memoryGPU2Override="1000"
-export clockGPU2Override="90"
+export clockGPU2Override="100"
 export memoryGPU3Override="1000"
-export clockGPU3Override="90"
+export clockGPU3Override="100"
 export memoryGPU4Override="1000"
-export clockGPU4Override="90"
+export clockGPU4Override="100"
 export memoryGPU5Override="1000"
-export clockGPU5Override="90"
+export clockGPU5Override="100"
 export memoryGPU6Override="1000"
-export clockGPU6Override="90"
+export clockGPU6Override="100"
