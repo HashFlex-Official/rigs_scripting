8528c0ccb4cf3ae39480dc8981ce068f88fd12ac
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-05-07 10:42:59 +0200

    test

--- fromCron.sh
+++ fromCron.sh
@@ -2,8 +2,7 @@
 ## wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/fromCron.sh | sh ; chmod +x ~/temp.sh ; sh ~/temp.sh  >> /tmp/watchdog.log 2>&1; rm ~/temp.sh
 
 export logPath="/tmp/watchdog.log"
-echo "REBOOOOOT!" >> /tmp/watchdog.log
-reboot
+#echo "REBOOOOOT!" >> /tmp/watchdog.log
 			
 echo "#! /bin/sh" > ~/temp.sh
 echo " "
