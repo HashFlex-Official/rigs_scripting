c9dcf15100fb02f5efda1011e39d231d09ae4364
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-05-07 09:37:00 +0200

    test reboot :/

--- fromCron.sh
+++ fromCron.sh
@@ -2,10 +2,8 @@
 ## wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/fromCron.sh | sh ; chmod +x ~/temp.sh ; sh ~/temp.sh  >> /tmp/watchdog.log 2>&1; rm ~/temp.sh
 
 export logPath="/tmp/watchdog.log"
-#echo "Arrêt du mining par manque de rentabilité." >> /tmp/watchdog.log
-
-#shutdown -h now 
-#shutdown -r
+echo "REBOOOOOT!" >> /tmp/watchdog.log
+reboot
 			
 echo "#! /bin/sh" > ~/temp.sh
 echo " "
