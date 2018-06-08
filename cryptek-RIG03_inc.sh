253ed83439bffd7dac0a6e99b619f2a81f2341e5
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-06-08 11:09:07 +0200

    decrease intensity

--- cryptek-RIG03_inc.sh
+++ cryptek-RIG03_inc.sh
@@ -1,3 +1,6 @@
 #systemctl reboot
 			# Restart mining rig
 #			/sbin/shutdown 2 -f -r
+echo "find /home/cryptek/claymore/ -name *log.txt -mtime +10 -exec rm {} \;" > $claymoreRunScript
+echo " " >> $claymoreRunScript
+echo " ./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x6b5C7a5316107b7Ac52034b0Ddd3145e2FbFe7d8/cryptek03/cryptekrigs@gmail.com -ttli 79 -wd 1 -gser 1 -etha 0 -r 120 -tt -90 -li $livalue -logfile /home/cryptek/claymore/log.txt " >> $claymoreRunScript
