bfcb85ae47857ea5e508fdace64d3df8f4b46be6
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-06-18 08:56:32 +0200

    test

--- cryptek-RIG03_inc.sh
+++ cryptek-RIG03_inc.sh
@@ -5,4 +5,4 @@
 # echo " " >> $claymoreRunScript
 # echo " ./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x6b5C7a5316107b7Ac52034b0Ddd3145e2FbFe7d8/cryptek03/cryptekrigs@gmail.com -ttli 79 -wd 1 -gser 1 -etha 0 -r 120 -tt -90 -li $livalue -logfile /home/cryptek/claymore/log.txt " >> $claymoreRunScript
 
-/sbin/shutdown 1 -f -r
+#/sbin/shutdown 1 -f -r
