28bf1f844489f1921e651e73865f01ff0ffaac04
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-06-28 14:31:18 +0200

    reboot

--- fromCron.sh
+++ fromCron.sh
@@ -2,9 +2,9 @@
 ## wget -O -  https://raw.githubusercontent.com/HashFlex-Official/rigs_scripting/master/fromCron.sh | sh ; chmod +x ~/temp.sh ; sh ~/temp.sh  >> /tmp/watchdog.log 2>&1; rm ~/temp.sh
 
 export logPath="/tmp/watchdog.log"
-#echo "HARD REBOOOOOT!" >> /tmp/watchdog.log
+echo "HARD REBOOOOOT!" >> /tmp/watchdog.log
 # Restart mining rig
-#/sbin/shutdown 1 -f -r
+/sbin/shutdown 1 -f -r
 			
 echo "#! /bin/sh" > ~/temp.sh
 echo " "
