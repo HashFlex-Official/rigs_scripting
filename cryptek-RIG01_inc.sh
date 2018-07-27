bd6d354a3e0fc4ef1a298a9aa572d30d7651a8b4
DELHAYE Fabien <FABIEN.DELHAYE@micropole.be>
2018-07-27 19:51:35 +0200

    upgrade claymore

--- cryptek-RIG01_inc.sh
+++ cryptek-RIG01_inc.sh
@@ -1,4 +1,20 @@
+if [ ! -d "/home/cryptek/claymore/newVersion" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+	cd /home/cryptek/claymore/
+	wget https://github.com/nanopool/Claymore-Dual-Miner/releases/download/v11.8/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak.AMD.NVIDIA.GPU.Miner.v11.8.-.LINUX.tar.gz
+	tar -zxvf ./Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak.AMD.NVIDIA.GPU.Miner.v11.8.-.LINUX.tar.gz
+	mv "./Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak AMD+NVIDIA GPU Miner v11.8 - LINUX/" ./newVersion
+	rm ./newVersion/dpools.txt
+    rm ./newVersion/epools.txt
+    rm ./mine.sh
 
-echo "find /home/cryptek/claymore/ -name *log.txt -mtime +10 -exec rm {} \;" > $claymoreRunScript
-echo " " >> $claymoreRunScript
-echo " ./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x6b5C7a5316107b7Ac52034b0Ddd3145e2FbFe7d8/cryptek01/cryptekrigs@gmail.com -ttli 79 -wd 1 -gser 1 -etha 0 -r 120 -tt -90 -li $livalue -logfile /home/cryptek/claymore/log.txt " >> $claymoreRunScript
+	ps aux | grep "mdS Mining" | awk '{print $2}' | xargs kill
+	
+	echo "find /home/cryptek/claymore/newVersion -name *log.txt -mtime +10 -exec rm {} \;" > $claymoreRunScript
+	echo " " >> $claymoreRunScript
+	echo " ./newVersion/ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x6b5C7a5316107b7Ac52034b0Ddd3145e2FbFe7d8/cryptek01/cryptekrigs@gmail.com -ttli 79 -wd 1 -gser 1 -etha 0 -r 120 -tt -90 -li $livalue -logfile /home/cryptek/claymore/log.txt " >> $claymoreRunScript
+fi
+
+
+
+
